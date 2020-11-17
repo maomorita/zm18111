c2dcad470c6530f99b4b60283701a2c363201195
maomorita <zm18111@josai.ac.jp>
2020-11-17 02:14:25 +0000

    1117

--- ex6-5.js
+++ ex6-5.js
@@ -1,4 +1,11 @@
-function sayhello65() 
-{varwho65 = 'world';alert('Hello, ' + who65 + '!');}
-function taro65() {who65 = '太郎';}
-function hanako65() {who65 = '花子';}
+var who65 = 'world';
+function sayhello65() {
+alert('Hello, ' + who65 + '!');
+}function taro65() {
+who = '太郎';}
+function hanako65() {
+who = '花子';}
+
+function sayhello66() 
+function taro66() {who66 = '太郎が来た';}
+function hanako66() {who66 = '花子が来た';}
