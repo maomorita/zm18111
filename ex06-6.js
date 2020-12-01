9ce11b2361e505c100945ce4e8f7794882245002
maomorita <zm18111@josai.ac.jp>
2020-12-01 02:18:43 +0000

    1201

---
+++ ex06-6.js
@@ -0,0 +1,7 @@
+function sayhello66() {
+    var who66 = 'world';
+    alert('Hello, ' + who66 + '!');}
+    function taro66() {
+        who65 = '太郎が来た';}
+    function hanako66() {
+        who65 = '花子が来た';}
