5cc0b9e699818c08541561b722584b9e1f030022
maomorita <zm18111@josai.ac.jp>
2020-12-05 16:29:41 +0000

    1206

--- ex6-1.js
+++ ex6-1.js
@@ -1,7 +1,4 @@
-function sayhello61() {alert('Hello, Function!');}
+function sayhello61() {
+    alert('Hello, Function!');
+}
 
-varwho = 'world';
-function sayhello() {
-alert('Hello, ' + who + '!');}
-function taro() {who = '太郎';
-function hanako() {who = '花子';}
