f79fe986cb90be8f65eacfd6a8c0152268d5a077
maomorita <zm18111@josai.ac.jp>
2021-01-12 02:07:11 +0000

    0112

--- ex6-6.js
+++ ex6-6.js
@@ -1,7 +1,8 @@
-function sayhello66() {
-    var who66 = 'world';
+
+        var who65 = 'world';
+    function sayhello66() {
     alert('Hello, ' + who66 + '!');}
     function taro66() {
-        who65 = '太郎が来た';}
-    function hanako66() {
-        who65 = '花子が来た';}
+        who66 = '太郎が来た';}
+        function hanako66() {
+            who66 = '花子が来た';}
